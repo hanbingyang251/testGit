2d19cb8270175f4158752c7b934ffa07da04bd78
han <407345010>
2020-10-15 13:58:13 +0800

    添加了两个方法

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,16 @@
 
 public class Hello {
 
+	Hello(){
+		System.out.println("构造函数");
+	}
+	public void test(){
+		System.out.println("现在是test方法");
+	}
 	public static void main(String[] args) {
 		System.out.println("更新1");
 		System.out.println("更新2");
-
+        
 	}
 
 }
